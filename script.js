330a931109f26523f35dd0814e3f56d921187a72
SofiaMesa <sofiajmesa@gmail.com>
2025-04-27 02:35:22 -0300

    Actualizacion automatica desde script

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 5000); // cambia cada 5 segundos
+
+// Detectar deslizamiento hacia la izquierda para cerrar el menú
+let touchStartX = 0;
+let touchEndX = 0;
+
+function handleTouchStart(e) {
+  touchStartX = e.changedTouches[0].screenX;
+}
+
+function handleTouchEnd(e) {
+  touchEndX = e.changedTouches[0].screenX;
+  handleGesture();
+}
+
+function handleGesture() {
+  if (touchStartX - touchEndX > 50) { // deslizar hacia la izquierda más de 50px
+    closeMenu();
+  }
+}
+
+// Agregar eventos al documento
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchend', handleTouchEnd, false);
+
+// Función que ya tenés para cerrar el menú
+function closeMenu() {
+  document.getElementById('sidebar').classList.remove('open');
+}
+
